cde7acb9a2068137843ce5894a73f8bc7d6632ac
Fadi Farid <54584256+Fadi-Farid@users.noreply.github.com>
2025-07-11 10:17:42 +0300

    Update Zap-scan.py

--- Zap-scan.py
+++ Zap-scan.py
@@ -31,3 +31,38 @@ print("✅ Scan results is generated.")
 report = []
 new_report = scan_results[:1]
 i=0
+for item in new_report:
+    i=i+1
+    description = item["description"]
+    reference = item["reference"]
+    solution_provided = item["solution"]
+    url = "http://3.236.245.215/api/generate"
+    payload = {
+     "model": "llama3",
+     "prompt": (
+      f"Vulnerability Reference: {reference}\n"
+      f"Description: {description}\n"
+      f"ZAP Recommendation: {solution_provided}\n"
+      f"Fix Required: Please provide the necessary code changes and specify the file name where the changes should be applied with the updated code in the repository '{code}'"
+     ),
+     "stream": False
+    }
+
+    solution = requests.post(url, json=payload)
+
+
+    response_json = solution.json()
+    readable_output = response_json["response"].encode().decode("unicode_escape")
+    print(readable_output)
+
+    report_entry = {
+        "Vulnerability_No": i,
+        "Name" : item["name"],
+        "url": item["url"],
+        "risk": item["risk"],
+        "tags" : item["tags"],
+        "solution": readable_output
+    }
+    print(f'✅ Solution is generated for Vulnerability_No: {i}.')
+    report.append(report_entry)
+# Save DAST report to file
